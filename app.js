569217c17d916bd79e83b5537f22993c703552c3
vivek7kushwah <vivekkushwah1412@gmail.com>
2025-03-13 22:31:50 +0530

    Fixed missing cors module

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post('/login',async(req,res)=>{
                 secure: true,    // Required for HTTPS
                 sameSite: "None" // Allows cross-origin cookies (Vercel → Railway)
             });
-            return res.render("data.ejs",{users:user});
+            return res.render("data.ejs",{users:{user}});
         } else {
            
             return res.status(401).send("something wents wrong...");
