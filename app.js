fd8b0a2c629a23b5064e7075b4af47c6ab24577f
vivek7kushwah <vivekkushwah1412@gmail.com>
2025-03-13 23:33:00 +0530

    Fixed missing cors module

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require("cors");
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 const cookieParser =require('cookie-parser');
 app.use(cookieParser());
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
-const fs= require('fs');
 const userModel = require('./usermodule');
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 app.use(express.static(path.join(__dirname,'public')));
 app.set('view.engine','ejs');
+const PORT = process.env.PORT || 3000;
 // const cors = require("cors");
 // app.use(cors({
 //     origin: "https://user-data-dun.vercel.app", // Replace with your Vercel domain
@@ -84,6 +89,6 @@ app.post('/login',async(req,res)=>{
 // app.listen(PORT, () => {
 //     console.log(`Server started on port ${PORT}`);
 // });
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log(`Server started`)
 })
