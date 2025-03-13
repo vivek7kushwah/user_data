f9e55f593210bcfdb6ad446dd5754aa762f996de
vivek7kushwah <vivekkushwah1412@gmail.com>
2025-03-13 18:21:09 +0530

    some modification

--- usermodule.js
+++ usermodule.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const env = require('dotenv')
 env.config();
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 const userSchema = mongoose.Schema({
     name : String ,
